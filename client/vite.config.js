23960352d013c3f1fded1b8bb6e3be0ce7a3ef9e
Irfan Mehmud <irfanmehmud140@gmail.com>
2025-04-15 10:41:27 +0530

    white washed backend ip

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://yardstick-internship-assesment-server.onrender.com',
         changeOrigin: true,
       },
     },
